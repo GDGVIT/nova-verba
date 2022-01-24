51ef2057707d165e9c7aa2b7d3c6e5ac280d2514
Pranjal Timsina <pranjal.timsina@live.com>
2022-01-24 17:51:34 +0545

    feat: styling for books page

--- frontend/src/screens/Book.tsx
+++ frontend/src/screens/Book.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 
+import Header from '../components/Header'
 import Word from '../components/Word'
 
 interface BookProps {
-  uiType: number
+  uiType: number,
+  bookName: string,
+  wordCount: number
 }
 
-const Book = ({ uiType }: BookProps) => {
+const Book = ({ uiType, bookName, wordCount=0 }: BookProps) => {
   const [uiState, setUiState] = useState(uiType)
-
+  // uiState 0 => Card
+  // uiState 1 => Screen
   const updateUiState = (newState: number) => {
     setUiState(() => newState)
   }
@@ -16,28 +20,31 @@ const Book = ({ uiType }: BookProps) => {
   switch (uiState) {
     case 0:
       return (
-        <div onClick={() => updateUiState(1)}>
-          <div>Book name</div>
-          <div>20 words</div>
+        <div className='book-card' onClick={() => updateUiState(1)}>
+          <div className='book-card-name'>{bookName}</div>
+          <div className='book-card-wordcount'>{wordCount} words</div>
         </div>
       )
     default:
       return (
-        <main>
-          <header>
-            Book Title
-            <div onClick={() => updateUiState(0)}>Back</div>
-          </header>
-          <div>
-            Sort: A to Z
-          </div>
-          <div>
-            <Word uiType={2} wordName='Good' wordMeaning='This is good stuff'/>
-            <Word uiType={2} wordName='Good' wordMeaning='This is good stuff'/>
-            <Word uiType={2} wordName='Good' wordMeaning='This is good stuff'/>
-            <Word uiType={2} wordName='Good' wordMeaning='This is good stuff'/>
-          </div>
-        </main>
+        <div className='overlay'>
+          <Header />
+          <main className='screen'>
+            <header className='books-header book-header'>
+              <span className='book-title' >{bookName}</span>
+              <button className='book-back-button' onClick={() => updateUiState(0)}>Back</button>
+            </header>
+            <div className='sort'>
+              Sort: A to Z
+            </div>
+            <div>
+              <Word uiType={2} wordName='Good' wordMeaning='This is good stuff'/>
+              <Word uiType={2} wordName='Good' wordMeaning='This is good stuff'/>
+              <Word uiType={2} wordName='Good' wordMeaning='This is good stuff'/>
+              <Word uiType={2} wordName='Good' wordMeaning='This is good stuff'/>
+            </div>
+          </main>
+        </div>
       )
   }
 }
--- frontend/src/screens/Main.tsx
+++ frontend/src/screens/Main.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 
-import { CircularProgressbar } from 'react-circular-progressbar'
-import 'react-circular-progressbar/dist/styles.css'
+import SearchResults from './SearchResults'
+import Books from './Books'
 
 import StreakCard from '../components/StreakCard'
 import ViewBookPageButton from '../components/ViewBookPageButton'
+
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 import '../styles/main.css'
 
 const Main = () => {
   const [searchQuery, setSearchQuery] = useState('')
+
   const handleSearchQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(() => e.target.value)
   }
@@ -18,18 +22,55 @@ const Main = () => {
     e.preventDefault()
   }
 
+  const [currentScreen, setCurrentScreen] = useState(0)
+
+  const updateCurrentScreen = (newScreen:number) => {
+    setCurrentScreen(() => newScreen)
+  }
+
   return (
-    <main id='main-screen' className='screen' >
-      <form id='search' onSubmit={handleSubmit}>
-        <input id='search-bar' type='text' placeholder='Search for a word' value={searchQuery} onChange={handleSearchQueryChange} />
-        <input id='search-submit' type='submit' />
-      </form>
-      <section id='main-screen-cards'>
-        <StreakCard cardTitle='Week Streak' cardText='6 of 7' cardIcon={<CircularProgressbar value={66} />}  optClassName='streak-card week-card'/>
-        <StreakCard cardTitle='Highest Streak' cardText='40' cardIcon={<CircularProgressbar value={66} />} optClassName='streak-card high-card' />
-        <StreakCard cardTitle='Your Books' cardText='12' cardIcon={<ViewBookPageButton />} optClassName='streak-card book-card'/>
-      </section>
-    </main>
+    <>
+      {currentScreen === 0 && (
+        <main id='main-screen' className='screen' >
+          <form id='search' onSubmit={handleSubmit}>
+            <input
+              id='search-bar'
+              type='text'
+              placeholder='Search for a word'
+              value={searchQuery}
+              onChange={handleSearchQueryChange}
+              />
+            <input id='search-submit' type='submit' />
+          </form>
+          <section id='main-screen-cards'>
+            <StreakCard
+              cardTitle='Week Streak'
+              cardText='6 of 7'
+              cardIcon={<CircularProgressbar value={66} />}
+              optClassName='streak-card week-card'
+              />
+            <StreakCard
+              cardTitle='Highest Streak'
+              cardText='40'
+              cardIcon={<CircularProgressbar value={66} />}
+              optClassName='streak-card high-card'
+              />
+            <StreakCard
+              cardTitle='Your Books'
+              cardText='12'
+              cardIcon={<ViewBookPageButton onClickFunction={updateCurrentScreen} />}
+              optClassName='streak-card main-book-card'
+              />
+          </section>
+        </main>
+      )}
+      {currentScreen === 1 && (
+        <Books getBackFunction={updateCurrentScreen} />
+      )}
+      {currentScreen === 2 && (
+        <SearchResults word={searchQuery} />
+      )}
+    </>
   )
 }
 
