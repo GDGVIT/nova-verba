55539f4546071cf59b00783d3d64deb89e912669
Aditya Mitra <mitraaditya1357@gmail.com>
2022-05-09 00:34:34 +0530

    fix: changed firebase config

--- Backend/api/firebase/firebase.js
+++ Backend/api/firebase/firebase.js
@@ -3,24 +3,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    secretKey : process.env.SECRET_KEY,
-    privateKey : process.env.PRIVATE_KEY,
-    privateKeyId : process.env.PRIVATE_KEY_ID,
-    clientId : process.env.CLIENT_ID,
-    clientEmail : process.env.CLIENT_EMAIL,
-    tokenUri: process.env.TOKEN_URI,
-    authUri: process.env.AUTH_URI,
-    authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    clientX509CertUrl: process.env.CLIENT_X509_CERT_URL
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id : process.env.PRIVATE_KEY_ID,
+    private_key : process.env.PRIVATE_KEY,
+    client_email : process.env.CLIENT_EMAIL,
+    client_id : process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
 };
 
 // Initialize Firebase
-admin.initializeApp(firebaseConfig);
-
-module.exports = admin
+admin.initializeApp({
+    credential: admin.credential.cert(firebaseConfig)
+  })
+  
+  module.exports = admin
