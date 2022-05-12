81efb37c1b9cfafd956c20f755353ac8a7c3b860
Aditya Mitra <mitraaditya1357@gmail.com>
2022-05-12 21:39:38 +0530

    fix: firebaseConfig

--- Backend/api/firebase/firebase.js
+++ Backend/api/firebase/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     type: process.env.TYPE,
     project_id: process.env.PROJECT_ID,
     private_key_id : process.env.PRIVATE_KEY_ID,
-    private_key : process.env.PRIVATE_KEY,
+    private_key : process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email : process.env.CLIENT_EMAIL,
     client_id : process.env.CLIENT_ID,
     auth_uri: process.env.AUTH_URI,
@@ -16,6 +16,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-admin.initializeApp(firebaseConfig);
+admin.initializeApp({
+    credential: admin.credential.cert(firebaseConfig)
+});
 
 module.exports = admin
